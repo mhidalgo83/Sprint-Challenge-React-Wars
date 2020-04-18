090a19e1f9db3d7d3533940b2bf132cb45dc0ac3
Matthew Hidalgo <mhidalgo83@outlook.com>
2020-04-18 10:59:08 -0700

    mvp complete

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from "react";
+import Character from "./components/Character";
 import axios from "axios";
 import "./App.css";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Header = styled.h1`
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+  width: 100%;
+`;
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -17,19 +29,21 @@ const App = () => {
         const data = res.data.results;
         setCharacters(data);
         // console.log(data);
-        
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-
   console.log("Characters", characters);
+
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <Wrapper className="App">
+      <Header className="Header">Characters</Header>
+      {characters.map((character) => {
+        return <Character key={character.id} character={character} />;
+      })}
+    </Wrapper>
   );
 };
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,46 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Image = styled.img`
+  width: 25%;
+  border-radius: 50%;
+  box-shadow: 10px 10px 60px 0px rgba(0, 0, 0, 0.5);
+`;
+const Header = styled.h1`
+  text-align: center;
+  width: 100%;
+`;
+
+const HeaderBackground = styled.div`
+  background: slategray;
+`;
+
+const Wrapper = styled.div`
+  width: 31%;
+  background: white;
+  margin: 2% auto;
+  border-radius: 5px;
+  box-shadow: 10px 10px 60px 0px rgba(0, 0, 0, 0.5);
+`;
+
+const Para = styled.p`
+  text-align: center;
+  border-top: 1px solid gray;
+  margin: 3% auto;
+  width: 80%;
+`;
+
+const Character = (props) => {
+  return (
+    <Wrapper>
+      <Header>{props.character.name}</Header>
+      <Image src={props.character.image} alt={props.character.name} />
+      <Para>Species: {props.character.species}</Para>
+      <Para>Status: {props.character.status}</Para>
+      <Para>Location: {props.character.location.name}</Para>
+    </Wrapper>
+  );
+};
+
+export default Character;
